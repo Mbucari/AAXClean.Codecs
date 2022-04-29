daed68d053f643b44df9bc35b7efc3c3ae54eff9
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-04-29 16:03:39 -0600

    Move tests dir

--- AAXClean.Codecs.Test/TestFiles.cs
+++ AAXClean.Codecs.Test/TestFiles.cs
@@ -13,7 +13,7 @@ namespace AAXClean.Codecs.Test
         private static List<FileStream> OpenTempFiles { get; } = new List<FileStream>();
         private static string HP_WebPath { get; } = "https://drive.google.com/uc?export=download&id=1UIc0ouxIspS2RjGstX1Rzvp3QdeidDrR&confirm=t";
 
-        private static string _HP_BookPath { get; } = @"..\..\..\..\..\..\TestFiles\Harry Potter and the Sorcerer's Stone, Book 1 [B017V4IM1G] - Zero.aax";
+        private static string _HP_BookPath { get; } = @"..\..\..\..\..\TestFiles\Harry Potter and the Sorcerer's Stone, Book 1 [B017V4IM1G] - Zero.aax";
 
         public static string HP_BookPath => FindOrDownload(_HP_BookPath, HP_WebPath);
 
