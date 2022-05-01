b0c82bc5ea234e22eb8a8778466eaaed399aa41e
Michael Bucari-Tovo <mbucari1@gmail.com>
2022-04-30 20:45:34 -0600

    Formatting

--- AAXClean.Codecs.Test/CodecTestBase.cs
+++ AAXClean.Codecs.Test/CodecTestBase.cs
@@ -1,126 +1,125 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 
 namespace AAXClean.Codecs.Test
 {
-    public abstract class CodecTestBase
-    {
-        private AaxFile _aax;
-        public AaxFile Aax
-        {
-            get
-            {
-                if (_aax is null)
-                {
-                    _aax = new AaxFile(File.Open(AaxFile, FileMode.Open, FileAccess.Read, FileShare.Read));
-                    _aax.SetDecryptionKey(new byte[16], new byte[16]);
-                }
-                return _aax;
-            }
-        }
-        public abstract string AaxFile { get; }
-        public abstract string SingleMp3Hash { get; }
-        public abstract TimeSpan SilenceDuration { get; }
-        public abstract List<(TimeSpan start, TimeSpan end)> SilenceTimes { get; }
-        public abstract List<string> MultiMp3Hashes { get; }
-        public abstract double SilenceThreshold { get; }
+	public abstract class CodecTestBase
+	{
+		private AaxFile _aax;
+		public AaxFile Aax
+		{
+			get
+			{
+				if (_aax is null)
+				{
+					_aax = new AaxFile(File.Open(AaxFile, FileMode.Open, FileAccess.Read, FileShare.Read));
+					_aax.SetDecryptionKey(new byte[16], new byte[16]);
+				}
+				return _aax;
+			}
+		}
+		public abstract string AaxFile { get; }
+		public abstract string SingleMp3Hash { get; }
+		public abstract TimeSpan SilenceDuration { get; }
+		public abstract List<(TimeSpan start, TimeSpan end)> SilenceTimes { get; }
+		public abstract List<string> MultiMp3Hashes { get; }
+		public abstract double SilenceThreshold { get; }
 
-        [TestMethod]
-        public void _0_SilenceDetection()
-        {
-            try
-            {
-                int silEndex = 0;
-                void SilenceDetected(SilenceDetectCallback callback)
-                {
-                    Assert.AreEqual(callback.Silence.SilenceStart, SilenceTimes[silEndex].start);
-                    Assert.AreEqual(callback.Silence.SilenceEnd, SilenceTimes[silEndex].end);
-                    silEndex++;
-                }
-                var silecnes = Aax.DetectSilence(SilenceThreshold, SilenceDuration, SilenceDetected).ToList();
+		[TestMethod]
+		public void _0_SilenceDetection()
+		{
+			try
+			{
+				int silEndex = 0;
+				void SilenceDetected(SilenceDetectCallback callback)
+				{
+					Assert.AreEqual(callback.Silence.SilenceStart, SilenceTimes[silEndex].start);
+					Assert.AreEqual(callback.Silence.SilenceEnd, SilenceTimes[silEndex].end);
+					silEndex++;
+				}
+				List<SilenceEntry> silecnes = Aax.DetectSilence(SilenceThreshold, SilenceDuration, SilenceDetected).ToList();
 
-                Assert.AreEqual(silecnes.Count, SilenceTimes.Count);
+				Assert.AreEqual(silecnes.Count, SilenceTimes.Count);
 
-                for (int i = 0; i < silecnes.Count; i++)
-                {
-                    Assert.AreEqual(silecnes[i].SilenceStart, SilenceTimes[i].start);
-                    Assert.AreEqual(silecnes[i].SilenceEnd, SilenceTimes[i].end);
-                }
+				for (int i = 0; i < silecnes.Count; i++)
+				{
+					Assert.AreEqual(silecnes[i].SilenceStart, SilenceTimes[i].start);
+					Assert.AreEqual(silecnes[i].SilenceEnd, SilenceTimes[i].end);
+				}
 
 #if DEBUG
                 StringBuilder sb = new StringBuilder();
                 foreach (var sil in silecnes)
                     sb.AppendLine($"(TimeSpan.FromTicks({sil.SilenceStart.Ticks}), TimeSpan.FromTicks({sil.SilenceEnd.Ticks})),");
 #endif
-            }
-            finally
-            {
-                Aax.Close();
-            }
-        }
+			}
+			finally
+			{
+				Aax.Close();
+			}
+		}
 
-        [TestMethod]
-        public void _1_ConvertMp3Single()
-        {
-            try
-            {
-                var tempfile = TestFiles.NewTempFile();
-                var result = Aax.ConvertToMp3(tempfile, new NAudio.Lame.LameConfig { Preset = NAudio.Lame.LAMEPreset.STANDARD_FAST, Mode = NAudio.Lame.MPEGMode.Mono });
+		[TestMethod]
+		public void _1_ConvertMp3Single()
+		{
+			try
+			{
+				FileStream tempfile = TestFiles.NewTempFile();
+				ConversionResult result = Aax.ConvertToMp3(tempfile, new NAudio.Lame.LameConfig { Preset = NAudio.Lame.LAMEPreset.STANDARD_FAST, Mode = NAudio.Lame.MPEGMode.Mono });
 
-                Assert.AreEqual(result, ConversionResult.NoErrorsDetected);
+				Assert.AreEqual(result, ConversionResult.NoErrorsDetected);
 
-                using var sha = SHA1.Create();
+				using SHA1 sha = SHA1.Create();
 
-                var mp4file = File.OpenRead(tempfile.Name);
-                int read;
-                byte[] buff = new byte[4 * 1024 * 1024];
+				FileStream mp4file = File.OpenRead(tempfile.Name);
+				int read;
+				byte[] buff = new byte[4 * 1024 * 1024];
 
-                while ((read = mp4file.Read(buff)) == buff.Length)
-                {
-                    sha.TransformBlock(buff, 0, read, null, 0);
-                }
-                mp4file.Close();
-                sha.TransformFinalBlock(buff, 0, read);
-                var fileHash = string.Join("", sha.Hash.Select(b => b.ToString("x2")));
+				while ((read = mp4file.Read(buff)) == buff.Length)
+				{
+					sha.TransformBlock(buff, 0, read, null, 0);
+				}
+				mp4file.Close();
+				sha.TransformFinalBlock(buff, 0, read);
+				string fileHash = string.Join("", sha.Hash.Select(b => b.ToString("x2")));
 
-                Assert.AreEqual(SingleMp3Hash, fileHash);
-            }
-            finally
-            {
-                TestFiles.CloseAllFiles();
-                Aax.Close();
-            }
-        }
+				Assert.AreEqual(SingleMp3Hash, fileHash);
+			}
+			finally
+			{
+				TestFiles.CloseAllFiles();
+				Aax.Close();
+			}
+		}
 
-        [TestMethod]
-        public void _2_ConvertMp3Multiple()
-        {
-            try
-            {
-                List<string> tempFiles = new();
-                void NewSplit(NewSplitCallback callback)
-                {
-                    callback.OutputFile = TestFiles.NewTempFile();
-                    tempFiles.Add(((FileStream)callback.OutputFile).Name);
-                }
+		[TestMethod]
+		public void _2_ConvertMp3Multiple()
+		{
+			try
+			{
+				List<string> tempFiles = new();
+				void NewSplit(NewSplitCallback callback)
+				{
+					callback.OutputFile = TestFiles.NewTempFile();
+					tempFiles.Add(((FileStream)callback.OutputFile).Name);
+				}
 
-                Aax.ConvertToMultiMp3(Aax.GetChapterInfo(), NewSplit, new NAudio.Lame.LameConfig { Preset = NAudio.Lame.LAMEPreset.STANDARD_FAST, Mode = NAudio.Lame.MPEGMode.Mono });
+				Aax.ConvertToMultiMp3(Aax.GetChapterInfo(), NewSplit, new NAudio.Lame.LameConfig { Preset = NAudio.Lame.LAMEPreset.STANDARD_FAST, Mode = NAudio.Lame.MPEGMode.Mono });
 #if !DEBUG
-                Assert.AreEqual(MultiMp3Hashes.Count, tempFiles.Count);
+				Assert.AreEqual(MultiMp3Hashes.Count, tempFiles.Count);
 #endif
-                using var sha = SHA1.Create();
-                List<string> hashes = new();
+				using SHA1 sha = SHA1.Create();
+				List<string> hashes = new();
 
-                foreach (var tmp in tempFiles)
-                {
-                    sha.ComputeHash(File.ReadAllBytes(tmp));
-                    hashes.Add(string.Join("", sha.Hash.Select(b => b.ToString("x2"))));
-                }
+				foreach (string tmp in tempFiles)
+				{
+					sha.ComputeHash(File.ReadAllBytes(tmp));
+					hashes.Add(string.Join("", sha.Hash.Select(b => b.ToString("x2"))));
+				}
 #if DEBUG
                 var hs = new StringBuilder();
 
@@ -130,16 +129,16 @@ namespace AAXClean.Codecs.Test
                 }
 #endif
 
-                for (int i = 0; i < tempFiles.Count; i++)
-                {
-                    Assert.AreEqual(MultiMp3Hashes[i], hashes[i]);
-                }
-            }
-            finally
-            {
-                TestFiles.CloseAllFiles();
-                Aax.Close();
-            }
-        }
-    }
+				for (int i = 0; i < tempFiles.Count; i++)
+				{
+					Assert.AreEqual(MultiMp3Hashes[i], hashes[i]);
+				}
+			}
+			finally
+			{
+				TestFiles.CloseAllFiles();
+				Aax.Close();
+			}
+		}
+	}
 }
--- AAXClean.Codecs.Test/TestFiles.cs
+++ AAXClean.Codecs.Test/TestFiles.cs
@@ -1,48 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AAXClean.Codecs.Test
 {
-    public static class TestFiles
-    {
-        private static readonly List<FileStream> OpenTempFiles = new List<FileStream>();
-        private const string TEST_FILE_DIR = @"..\..\..\..\..\TestFiles";
-        private const string HP_URL = "https://drive.google.com/uc?export=download&id=1UIc0ouxIspS2RjGstX1Rzvp3QdeidDrR&confirm=t";
-        private static readonly string HP_FILENAME = Path.Combine(TEST_FILE_DIR, "HP_Zero.aax");
-        public static string HP_BookPath => FindOrDownload(HP_FILENAME, HP_URL);
+	public static class TestFiles
+	{
+		private static readonly List<FileStream> OpenTempFiles = new List<FileStream>();
+		private const string TEST_FILE_DIR = @"..\..\..\..\..\TestFiles";
+		private const string HP_URL = "https://drive.google.com/uc?export=download&id=1UIc0ouxIspS2RjGstX1Rzvp3QdeidDrR&confirm=t";
+		private static readonly string HP_FILENAME = Path.Combine(TEST_FILE_DIR, "HP_Zero.aax");
+		public static string HP_BookPath => FindOrDownload(HP_FILENAME, HP_URL);
 
-        private static string FindOrDownload(string path, string url)
-        {
-            if (!File.Exists(path))
-            {
-                if (!Directory.Exists(TEST_FILE_DIR))
-                    Directory.CreateDirectory(TEST_FILE_DIR);
-                using WebClient cli = new();
-                cli.DownloadFile(url, path);
-            }
-            return path;
-        }
+		private static string FindOrDownload(string path, string url)
+		{
+			if (!File.Exists(path))
+			{
+				if (!Directory.Exists(TEST_FILE_DIR))
+					Directory.CreateDirectory(TEST_FILE_DIR);
+				using WebClient cli = new();
+				cli.DownloadFile(url, path);
+			}
+			return path;
+		}
 
-        public static void CloseAllFiles()
-        {
-            foreach (var fs in OpenTempFiles)
-            {
-                fs.Close();
-                File.Delete(fs.Name);
-            }
-            OpenTempFiles.Clear();
-        }
+		public static void CloseAllFiles()
+		{
+			foreach (FileStream fs in OpenTempFiles)
+			{
+				fs.Close();
+				File.Delete(fs.Name);
+			}
+			OpenTempFiles.Clear();
+		}
 
-        public static FileStream NewTempFile()
-        {
-            var fs = File.Open(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
-            OpenTempFiles.Add(fs);
-            return fs;
-        }
-    }
+		public static FileStream NewTempFile()
+		{
+			FileStream fs = File.Open(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+			OpenTempFiles.Add(fs);
+			return fs;
+		}
+	}
 }
